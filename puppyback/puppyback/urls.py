f3319ea3fae8c28e4b74150629654396b34c564f
Zach Estus <zachary.estus@student.awesomeinc.org>
2022-12-12 15:05:58 +0000

    Readying for deployment

--- puppyback/puppyback/urls.py
+++ puppyback/puppyback/urls.py
@@ -5,3 +5,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('database.urls'))
 ]
+
+from django.conf.urls.static import static
+from django.conf import settings
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
